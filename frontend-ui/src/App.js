3b55e6d800ee1fd7aa7127f28572fb8fc2fb8d71
Virginia Dooley <vcdooley@gmail.com>
2019-11-14 16:21:33 +0000

    remove boilerplate and import journies component

--- frontend-ui/src/App.js
+++ frontend-ui/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Journies from './Journies'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         
-      </header>
+          <Journies/>
+
     </div>
+    
   );
 }
 
