1e672ac8be0dc2768afa61959117b1353115184a
Virginia Dooley <vcdooley@gmail.com>
2019-11-14 17:02:55 +0000

    seed file

--- backend-api/db/seeds.rb
+++ backend-api/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Client.create(business_name: 'GAP');
+Courier.create(client_id: 1, courier_name: 'FedEx');
+Journey.create([{ courier_id: 1, vehincle_type: "Van (Class I)", 
+shipping_speed: "next day", leg: "A", address_1: "30 Lancaster Road", 
+address_2: "Flat 4", city: "London", 
+post_code: "W11", country: "UK", distance: 100}]);
