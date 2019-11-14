700f051da1b6287527c8db28965ffbc16f088ced
Virginia Dooley <vcdooley@gmail.com>
2019-11-14 16:21:56 +0000

    create journies component w basic structure

---
+++ frontend-ui/src/Journies.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Journies extends Component {
+
+  state = {
+      journies: []
+  }
+
+  componentDidMount() {
+    
+  }
+
+  
+
+   render() {
+      
+      return (
+
+        <div className="grid-container">
+          <div className="grid-item">
+
+            <h3>Journies:</h3>
+
+          </div>
+        </div>
+
+      )
+  }
+}
+
+export default Journies;
